46ef25feb1fb2d388ee09df3c1a1fd3a1bd02a28
abdy6 <abdurrahman1106@icloud.com>
2025-06-11 14:37:05 -0400

    Added config file

--- bot.py
+++ bot.py
@@ -1,11 +1,12 @@
 import os
-import discord
-from discord.ext import commands
 from dotenv import load_dotenv
-import definitions
 import time
 import datetime
 import logging
+from definitions import load_config, Config
+
+import discord
+from discord.ext import commands
 
 logger = logging.getLogger(__name__)
 
@@ -20,11 +21,17 @@ extensions = [
 
 
 class BrethrenBot(commands.Bot):
-    def __init__(self):
+    """BrethrenBot class."""
+    def __init__(self, bot_config: Config):
         intents = discord.Intents.default()
         intents.message_content = True
+        self.config = bot_config
 
-        super().__init__(command_prefix=definitions.COMMAND_PREFIX, intents=intents)
+        self.start_time = datetime.datetime.now()
+        # Used to count the bot's uptime
+        self.monotonic_start_time = time.monotonic()
+
+        super().__init__(command_prefix=self.config.command_prefix, intents=intents)
 
     async def setup_hook(self):
         # Load cogs
@@ -36,18 +43,21 @@ class BrethrenBot(commands.Bot):
         try:
             synced_commands = await self.tree.sync()
             print(f"Synced {len(synced_commands)} commands: {[cmd.name for cmd in synced_commands]}")
-            logger.debug(f"Synced {len(synced_commands)} commands: {[cmd.name for cmd in synced_commands]}")
+            logger.debug("Synced %s commands: %s", len(synced_commands), [cmd.name for cmd in synced_commands])
         except Exception as e:
             logger.error("Error", exc_info=True)
 
     async def on_ready(self):
         print(f"Logged in as {self.user} (ID: {self.user.id})")
-        
-        self.start_time = datetime.datetime.now()
-        # Used to count the bot's uptime
-        self.monotonic_start_time = time.monotonic()
 
-def run_bot():
+
+if __name__ == '__main__':
+    logging.basicConfig(
+        filename='brethren_bot.log', 
+        level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s'
+    )
+    config = load_config()
+
     token = os.getenv("BOT_TOKEN")
-    bot = BrethrenBot()
+    bot = BrethrenBot(bot_config=config)
     bot.run(token)
--- definitions.py
+++ definitions.py
@@ -1,11 +1,30 @@
+import json
+import logging
 from discord.ext import commands
 
+logger = logging.getLogger(__name__)
 
-COMMAND_PREFIX = ";"
 
+class Config:
+    def __init__(self, data):
+        self.command_prefix = data.get('command_prefix', ';') # Default value: ";"
+        self.owner_id = data.get('owner_id', 424970840015110145) # Default owner: adam.2006
+
+
+def load_config(path='config.json') -> Config:
+    """Return the bot's configuration."""
+    with open(path, 'r') as file:
+        data = json.load(file)
+
+    logger.debug(f"Loaded config: {data}")
+    print(f"Loaded config: {data}")
+    return Config(data=data)
+
+
+_config = load_config()
 
 def is_bot_owner():
     """Check whether command issuer is me (adam.2006)"""
     def predicate(ctx):
-        return ctx.message.author.id == 424970840015110145
+        return ctx.message.author.id == _config.owner_id
     return commands.check(predicate)
